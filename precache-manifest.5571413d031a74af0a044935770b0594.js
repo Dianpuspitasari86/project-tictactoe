2006b92652676d7b6f8b18577c05d7e9385c8cd1
Dianpuspitasari86 <dianpuspitasari171586@gmail.com>
2019-11-18 10:28:53 +0700

    Updates

---
+++ precache-manifest.5571413d031a74af0a044935770b0594.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307ad3f34661c2fd278457fc6ba1be44",
+    "url": "/project-tictactoe/index.html"
+  },
+  {
+    "revision": "65d52c7472b8e5f81402",
+    "url": "/project-tictactoe/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "c8c4d63f005c1cf7880c",
+    "url": "/project-tictactoe/static/js/2.246d8606.chunk.js"
+  },
+  {
+    "revision": "65d52c7472b8e5f81402",
+    "url": "/project-tictactoe/static/js/main.35a57e3e.chunk.js"
+  },
+  {
+    "revision": "10e3eed0aab3fd17f8e1",
+    "url": "/project-tictactoe/static/js/runtime-main.7ffa37fe.js"
+  }
+]);
